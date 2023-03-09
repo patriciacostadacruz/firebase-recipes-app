2bb2c858a46fc65c1f8c572e8f1d299ceef27df7
patriciacostadacruz <patriciacstcz@gmail.com>
2023-03-09 12:02:40 +0100

    Adds read logic

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import firebaseAuthService from "./FirebaseAuthService";
 import LoginForm from "./components/LoginForm";
 import firebaseFirestoreService from "./FirebaseFirestoreService";
 import AddEditRecipeForm from "./components/AddEditRecipeForm";
+// eslint-disable-next-line no-unused-vars
+import firebase from "./FirebaseConfig";
 
 import "./App.css";
 
 function App() {
   const [user, setUser] = useState(null);
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetchRecipes()
+      .then((fetchedRecipes) => {
+        setRecipes(fetchedRecipes);
+      })
+      .catch((error) => {
+        console.error(error.message);
+        throw error;
+      });
+  }, [user]);
 
   // listens to any change in auth, assigns new value
   firebaseAuthService.subscribeToAuthChanges(setUser);
+
+  const fetchRecipes = async () => {
+    let fetchedRecipes = [];
+    try {
+      const response = await firebaseFirestoreService.readDocuments("recipes");
+      const newRecipes = response.docs.map((recipeDoc) => {
+        const id = recipeDoc.id;
+        const data = recipeDoc.data();
+        data.publishDate = new Date(data.publishDate.seconds * 1000);
+        return { ...data, id };
+      });
+      fetchedRecipes = [...newRecipes];
+    } catch (error) {
+      console.error(error.message);
+      throw error;
+    }
+    return fetchedRecipes;
+  };
+
+  const handleFetchRecipes = async () => {
+    try {
+      const fetchedRecipes = await fetchRecipes();
+      setRecipes(fetchedRecipes);
+    } catch (error) {
+      console.error(error.message);
+      throw error;
+    }
+  };
 
   const handleAddRecipe = async (newRecipe) => {
     try {
@@ -18,6 +60,7 @@ function App() {
         "recipes",
         newRecipe
       );
+      handleFetchRecipes();
       alert(`Successfully created a recipe with an ID = ${response.id}`);
     } catch (error) {
       alert(error.message);
@@ -31,7 +74,28 @@ function App() {
         <LoginForm existingUser={user} />
       </div>
       <div className="main">
-        <AddEditRecipeForm handleAddRecipe={handleAddRecipe} />
+        <div className="center">
+          <div className="recipe-dash-box">
+            {recipes && recipes.length > 0 ? (
+              <div className="recipe-list">
+                {recipes.map((recipe) => {
+                  return (
+                    <div className="recipe-card" key={recipe.id}>
+                      <div className="recipe-name">{recipe.name}</div>
+                      <div className="recipe-field">
+                        Category: {recipe.category}
+                      </div>
+                      <div className="recipe-field">
+                        Publish date: {recipe.publishDate.toString()}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            ) : null}
+          </div>
+        </div>
+        {user ? <AddEditRecipeForm handleAddRecipe={handleAddRecipe} /> : null}
       </div>
     </div>
   );
--- src/FirebaseFirestoreService.js
+++ src/FirebaseFirestoreService.js
@@ -6,8 +6,13 @@ const createDocument = (collection, document) => {
   return Firestore.collection(collection).add(document);
 };
 
+const readDocuments = (collection) => {
+  return Firestore.collection(collection).get();
+};
+
 const firebaseFirestoreService = {
   createDocument,
+  readDocuments,
 };
 
 export default firebaseFirestoreService;
